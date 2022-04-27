c605456cfca4e52da7e9c68b1b11da33d30a157e
Mariah <mariahschock@gmail.com>
2022-04-27 15:56:14 -0700

    middleDropdown

--- app.js
+++ app.js
@@ -13,14 +13,21 @@ const catchphraseButton = document.getElementById('catchphrase-button');
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
 
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
+
+let catchphrases = [];
+
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
+    console.log('changing head', headDropdown.value);
     // increment the head change count state
-    
+    headCount++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url('./assets/${headDropdown.value}.png`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
